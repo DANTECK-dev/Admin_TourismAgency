6d11d2be486ff2720ff7cde8b8954de9be3ba23a
Александр Немеров <nemerov_a@mail.ru>
2023-06-21 04:55:36 +0800

    Done

--- Admin_TourismAgency/MainWindow.xaml.cs
+++ Admin_TourismAgency/MainWindow.xaml.cs
@@ -20,9 +20,37 @@ namespace Admin_TourismAgency
     /// </summary>
     public partial class MainWindow : Window
     {
+        string _login = "RwNydpKbI@O{nk8V{7I|p2hW#%IzGxS?7fDA4R2dkb6R21rt3@Kq3KgJx8}fE2vc~VmVkKV0Q2%b?WyMSEAQSh$iqE~qkk1RH#q" +
+            "jSS~5s1QXMpCN32A%$U|fLMTX$TbFG?PSasfzHkEJBeN~NEKPXE{{H$Qed$pe3ly2PnzT?L5WsGmFP}uOY*CQ0djBhO19Rx9HSkywg~xSQr47OP";
+        string _password = "#I#lvGKDJMM%B2tbi4Hj}~aeNVwKB~N9~ltC$tlBiI{K|gg{1~~IlMH~nWq%}h#2*%@Xk}cg1e{Hh%EazDIjcU88v1tQYK$|" +
+            "Yg@qkxB3{kG~|JV~*gRcefY$OXrOyPAn97ionSaz#VgL92~87pVjX{4G1pLgi||2C@0S$y#}dKrZ~2ai|msCnFbXwS8J1q7R2G8nB%apGtNbzv@" +
+            "dtQ69b5fNMSAcFA1wO3vW$8mXNa5F6rDtHYTonmltRxanu8x?5eTYa$iLF4DFC72323LvIRbyN#K0q?|wHBdwyW#Y%2Oun@6Qd#YWMv}ldNj*fT";
+
         public MainWindow()
         {
             InitializeComponent();
+            Login_G.Visibility = Visibility.Visible;
+            Registered_G.Visibility = Visibility.Hidden;
+            Login_TB.Text = "";
+            Password_TB.Text = "";
+        }
+
+        private void Login_TB_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(Login_TB.Text == _login && Password_TB.Text == _password)
+            {
+                Login_G.Visibility = Visibility.Hidden;
+                Registered_G.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Password_TB_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (Login_TB.Text == _login && Password_TB.Text == _password)
+            {
+                Login_G.Visibility = Visibility.Hidden;
+                Registered_G.Visibility = Visibility.Visible;
+            }
         }
 
         private void OpenViewRoutes(object sender, RoutedEventArgs e)
