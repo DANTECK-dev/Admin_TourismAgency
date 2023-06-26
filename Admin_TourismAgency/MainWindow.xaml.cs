5ad6151229b777f7eae2a40a0693a7ee4cf8ad46
Александр Немеров <nemerov_a@mail.ru>
2023-06-26 18:46:08 +0800

    asd

--- Admin_TourismAgency/MainWindow.xaml.cs
+++ Admin_TourismAgency/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
 
 namespace Admin_TourismAgency
 {
@@ -9,11 +12,14 @@ namespace Admin_TourismAgency
     public partial class MainWindow : Window
     {
         // логин и пароль
-        string _login = "RwNydpKbI@O{nk8V{7I|p2hW#%IzGxS?7fDA4R2dkb6R21rt3@Kq3KgJx8}fE2vc~VmVkKV0Q2%b?WyMSEAQSh$iqE~qkk1RH#q" +
+        /*string _login = "RwNydpKbI@O{nk8V{7I|p2hW#%IzGxS?7fDA4R2dkb6R21rt3@Kq3KgJx8}fE2vc~VmVkKV0Q2%b?WyMSEAQSh$iqE~qkk1RH#q" +
             "jSS~5s1QXMpCN32A%$U|fLMTX$TbFG?PSasfzHkEJBeN~NEKPXE{{H$Qed$pe3ly2PnzT?L5WsGmFP}uOY*CQ0djBhO19Rx9HSkywg~xSQr47OP";
         string _password = "#I#lvGKDJMM%B2tbi4Hj}~aeNVwKB~N9~ltC$tlBiI{K|gg{1~~IlMH~nWq%}h#2*%@Xk}cg1e{Hh%EazDIjcU88v1tQYK$|" +
             "Yg@qkxB3{kG~|JV~*gRcefY$OXrOyPAn97ionSaz#VgL92~87pVjX{4G1pLgi||2C@0S$y#}dKrZ~2ai|msCnFbXwS8J1q7R2G8nB%apGtNbzv@" +
-            "dtQ69b5fNMSAcFA1wO3vW$8mXNa5F6rDtHYTonmltRxanu8x?5eTYa$iLF4DFC72323LvIRbyN#K0q?|wHBdwyW#Y%2Oun@6Qd#YWMv}ldNj*fT";
+            "dtQ69b5fNMSAcFA1wO3vW$8mXNa5F6rDtHYTonmltRxanu8x?5eTYa$iLF4DFC72323LvIRbyN#K0q?|wHBdwyW#Y%2Oun@6Qd#YWMv}ldNj*fT";*/
+
+        string _login = "admin";
+        string _password = "admin";
 
         public MainWindow()
         {
@@ -24,13 +30,40 @@ namespace Admin_TourismAgency
             // очищаем форму
             Login_TB.Text = "";
             Password_TB.Text = "";
+
+            //this.Background = new ImageBrush(new BitmapImage(new Uri("C:/Images/bg1.png")));
+        }
+
+        private void BG_Image_BT_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+
+
+            // Set filter for file extension and default file extension
+            dlg.DefaultExt = ".png";
+            dlg.Filter = "PNG Files (*.png)|*.png|JPEG Files (*.jpeg)|*.jpeg|JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif|All Files (*.*)|*.*";
+
+
+            // Display OpenFileDialog by calling ShowDialog method
+            Nullable<bool> result = dlg.ShowDialog();
+
+
+            // Get the selected file name and display in a TextBox
+            if (result == true)
+            {
+                // Open document
+                string filename = dlg.FileName;
+                this.Background = new ImageBrush(new BitmapImage(new Uri(filename)));
+            }
         }
 
         private void Login_TB_TextChanged(object sender, TextChangedEventArgs e)
-        {        
+        {
             // при вводе логина проверяется правильность ввдененых данных и если все правильно,
             // то открывается форма авторизированного админа, а форма входа скрывается
-            if(Login_TB.Text == _login && Password_TB.Text == _password)
+            if (Login_TB.Text == _login && Password_TB.Text == _password)
             {
                 Login_G.Visibility = Visibility.Hidden;
                 Registered_G.Visibility = Visibility.Visible;
